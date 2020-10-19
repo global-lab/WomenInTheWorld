36b7796de13aca041a1ba2791d0a6ca902ed5692
lokesh234 <lgangaramaney@wpi.edu>
2020-10-19 15:53:49 -0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/WomenInTheWorld/precache-manifest.8f3952931e4e696a537f58d600cb722c.js"
+  "/WomenInTheWorld/precache-manifest.ab1f91171503b51198b1dc3e3a6a2e9d.js"
 );
 
 self.addEventListener('message', (event) => {
